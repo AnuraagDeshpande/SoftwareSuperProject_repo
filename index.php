9655d74f51402d20cdc0edbfa60e58fd0b1a5b48
tpdkrt <tpodko@gmail.com>
2025-02-28 21:38:12 +0100

    subheaders added

--- index.php
+++ index.php
@@ -73,6 +73,12 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod.
                 </div>
             </div>
+            <h1 class="section-header">
+                Fresh design and useful features
+            </h1>
+            <h1 class="section-header">
+                Meet out team
+            </h1>
         </div>
     </body>
 </html>
