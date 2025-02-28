6e5beafaa1f389fedfe542809ae5871260665ccb
tpdkrt <tpodko@gmail.com>
2025-02-28 20:34:09 +0100

    adapted the main page look to be closer to the reference

--- index.php
+++ index.php
@@ -6,51 +6,45 @@
         <title>SE project</title>
         <link href="styles/main.css" rel="stylesheet"/>
         <link href="styles/sidebar.css" rel="stylesheet"/>
+        <link href="styles/navbar.css" rel="stylesheet"/>
         <link href="styles/index.css" rel="stylesheet"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script type="text/javascript" src="scripts/sidebar.js"></script>
     </head>
     <body>
+        <!--NAVIGATION BAR CODE-->
         <div class="navbar">
-            
-        </div>
-        <div class="sidebar">
-            <button class=sidebar-cell id="hamburger" onclick="
+            <button id="hamburger" onclick="
                 hideSidebar();
             ">
- 
                 <i class="fa fa-bars" aria-hidden="true"></i>
-                <div class="sidebar-label js-sidebar-hide">
-                    Menu
-                </div>
             </button>
-            <h1 class="js-sidebar-hide">PMBOK</h1>
-            <div class="sep js-sidebar-hide"></div>
-            <h2 class="sidebar-subheader js-sidebar-hide">General</h2>
-            <button class="sidebar-cell js-sidebar-hide">
+        </div>
+        <!--SIDEBAR CODE-->
+        <div class="sidebar">
+            <h1>PMBOK</h1>
+            <div></div>
+            <h2 class="sidebar-subheader">General</h2>
+            <button class="sidebar-cell">
                 <i class="fa fa-database" aria-hidden="true"></i>
                 <div class="sidebar-label">
                     Projects
                 </div>
             </button>
-            <button class="sidebar-cell js-sidebar-hide">
+            <button class="sidebar-cell">
                 <i class="fa fa-users" aria-hidden="true"></i>
                 <div class="sidebar-label">
                     Team
                 </div>
             </button>
         </div>
-        <div class="content">
-            <div class="header">
-                <div class="header-text">
-                    <h3>Project Management Boook of Knowledge based</h3>
-                    <h1>Manage your project more efficiently!</h1>
-                </div>
-                <img src="media/index_header.jpg">
-               
+        <div class="header">
+            <div class="header-text">
+                
+                <h1>Manage your project more efficiently!</h1>
+                <h3>Project Management Boook of Knowledge based</h3>
             </div>
-            
-        </div>
-        
+            <img src="media/index_header.jpg">
+        </div>        
     </body>
 </html>
