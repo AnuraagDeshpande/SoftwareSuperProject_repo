c2ae796d9344125dec1dfd3a6d04daafe4b52a25
tpdkrt <tpodko@gmail.com>
2025-02-28 21:34:35 +0100

    review section added

--- index.php
+++ index.php
@@ -14,11 +14,14 @@
     <body>
         <!--NAVIGATION BAR CODE-->
         <div class="navbar">
-            <button id="hamburger" onclick="
-                hideSidebar();
-            ">
-                <i class="fa fa-bars" aria-hidden="true"></i>
-            </button>
+            <div>
+                <button id="hamburger" onclick="
+                    hideSidebar();
+                ">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                NAME
+            </div>
             <button class="dark-navbar-button">
                 Sign Up
             </button>
@@ -41,6 +44,7 @@
                 </div>
             </button>
         </div>
+        <!--PAGE BODY AND COTNENT-->
         <div class="header">
             <div class="header-text">
                 
@@ -48,6 +52,27 @@
                 <h3>Project Management Boook of Knowledge based</h3>
             </div>
             <img src="media/index_header.jpg">
-        </div>        
+        </div>
+        <div class="content-box">
+            <h1>
+                Beloved by imaginary users!
+            </h1> 
+            <div class="review-grid">
+                <div class="review">
+                    <div class="review-header">
+                        <img src="media/profile-picture-placeholder.png">
+                        by Pikachu
+                    </div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod.
+                </div>
+                <div class="review">
+                    <div class="review-header">
+                        <img src="media/profile-picture-placeholder.png">
+                        by Beloved Client
+                    </div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod.
+                </div>
+            </div>
+        </div>
     </body>
 </html>
