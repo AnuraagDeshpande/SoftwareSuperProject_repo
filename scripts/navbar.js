bfa148100f66380a2bb084250865ef12aaa20b23
tpdkrt <tpodko@gmail.com>
2025-03-14 13:55:19 +0100

    added generate navbar tests

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,22 +1,24 @@
 function generateNavbar(){
     const navbar=document.querySelector(".navbar");
-    navbar.innerHTML= `
-        <div>
-        <button id="hamburger" onclick="
-            hideSidebar();
-        ">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-        </button>
-        <a href="./index.php">
-        NAME
-        </a>
-        </div>
-        <a href="login.html">
-            <button class="dark-button">
-                    Sign Up
+    if(navbar){
+        navbar.innerHTML= `
+            <div>
+            <button id="hamburger" onclick="
+                hideSidebar();
+            ">
+                <i class="fa fa-bars" aria-hidden="true"></i>
             </button>
-        </a>
-    `;
+            <a href="./index.php">
+            NAME
+            </a>
+            </div>
+            <a href="login.html">
+                <button class="dark-button">
+                        Sign Up
+                </button>
+            </a>
+        `;
+    }
 }
 
 generateNavbar();
