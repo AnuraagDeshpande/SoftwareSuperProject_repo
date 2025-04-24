8747bd235ef106e7345d6aec96449203a29bca38
unknown <dulguunulzii3@gmail.com>
2025-04-24 21:05:11 +0200

    Delete files from the main directory

--- scripts/navigation.js
+++
@@ -1,7 +0,0 @@
-import { generateNavbar } from "./navbar.js";
-import { generateSidebar, hideSidebar } from "./sidebar.js";
-
-generateNavbar();
-generateSidebar();
-
-window.hideSidebar=hideSidebar;
