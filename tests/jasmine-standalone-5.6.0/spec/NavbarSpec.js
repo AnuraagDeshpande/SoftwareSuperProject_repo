1f33d3c9d8f4f9b78f36e9d1eb6b1fd19564440b
tpdkrt <tpodko@gmail.com>
2025-05-09 10:04:16 +0200

    fixed navbar tests

--- tests/jasmine-standalone-5.6.0/spec/NavbarSpec.js
+++ tests/jasmine-standalone-5.6.0/spec/NavbarSpec.js
@@ -7,15 +7,15 @@ describe("NAVBAR", ()=>{
         testDiv.innerHTML="<div class=\"navbar\"></div>";
     });
 
-    afterEach(()=>{
-        testDiv.innerHTML="<div class=\"navbar\"></div>";
+    afterAll(()=>{
+        testDiv.innerHTML="";
     });
 
     //is the div no longer empty?
     it("generate navbar basic test",()=>{
         const navbar= document.querySelector(".navbar");
         expect(navbar.innerHTML).toEqual("");
-        generateNavbar();
+        generateNavbar(false, "name");
         expect(navbar.innerHTML).not.toEqual("");
     });
 
@@ -23,7 +23,7 @@ describe("NAVBAR", ()=>{
     it("hamburger menu present",()=>{
         let ham= document.getElementById("hamburger");
         expect(ham).toEqual(null);
-        generateNavbar();
+        generateNavbar(false, "name");
         ham= document.getElementById("hamburger");
         expect(ham.innerHTML).not.toEqual(null);
     });
@@ -32,7 +32,7 @@ describe("NAVBAR", ()=>{
     it("login page link persent",()=>{
         let button = document.querySelector('#btn-login');
         expect(button).toEqual(null);
-        generateNavbar();
+        generateNavbar(false, "name");
         button = document.querySelector('#btn-login');
         expect(button).not.toEqual(null);
     }); 
