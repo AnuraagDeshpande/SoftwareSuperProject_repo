ff57e6198699b58664ef9154a871b933b8b85769
Alen <akostov@constructor.university>
2025-05-11 11:26:32 +0200

    permission changes

--- Backend/api/routes/tasks.php
+++ Backend/api/routes/tasks.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 require_once(__DIR__ . '/../controllers/task_controller.php');
 
--- Backend/api/routes/users.php
+++ Backend/api/routes/users.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 require_once(__DIR__ . '/../controllers/user_controller.php');
 
