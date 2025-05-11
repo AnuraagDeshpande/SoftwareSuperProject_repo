24d9f7ac9824aacaa833d07bf24a4b70c889fef9
Alen <akostov@constructor.university>
2025-05-11 12:13:31 +0200

    Hopefully fixed allowed requests

--- Backend/api/index.php
+++ Backend/api/index.php
@@ -1,6 +1,6 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+  header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
   header("Access-Control-Allow-Headers: Content-Type, Authorization");
   
   // For preflight requests
