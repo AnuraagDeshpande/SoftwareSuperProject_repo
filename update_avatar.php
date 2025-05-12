f50b07e29a85a0b0e7bb319d911b990cc43d0177
Hikmat Vugarli <127110045+RomanusH@users.noreply.github.com>
2025-05-12 18:33:29 +0200

    Update update_avatar.php, i just updated directory, works totally fine

--- update_avatar.php
+++ update_avatar.php
@@ -4,7 +4,7 @@ require 'db_connection.php';
 
 $user_id = $_SESSION['user_id'];
 $avatar = $_POST['avatar']; // Expected values: avatar1.png, avatar2.png, etc.
-$avatar_path = "media/" . basename($avatar);
+$avatar_path = "avatars/" . basename($avatar);
 
 $stmt = $conn->prepare("UPDATE users SET profile_picture = ? WHERE id = ?");
 $stmt->bind_param("si", $avatar_path, $user_id);
